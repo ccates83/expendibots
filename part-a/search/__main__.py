f6648f48a4b5ddd0d58248fc7af82e093b591b0e
Connor Cates <connorcates@Connors-MBP.fios-router.home>
2020-03-29 10:18:54 -0400

    basic greedy algorithm that solves a 1v1 and prints the moves

--- part-a/search/__main__.py
+++ part-a/search/__main__.py
@@ -22,18 +22,23 @@ def solve():
     """
 
 
+def horizontal_distance(node1, node2):
+    return abs(node1[1] - node2[1])
+
+
+def vertical_distance(node1, node2):
+    return abs(node1[2] - node2[2])
+
+
 def calculate_number_moves(node, target_node):
     """
     Calculates the number of moves it would take the node to reach the distination
     coordinates. Works with stacks.
-
     Should not have to worry about pieces in the way.
     """
-    horizontal_distance = abs(node[1] - target_node[1])
-    vertical_distance = abs(node[2] - target_node[2])
-
     # distance / number of spaces the stack can move
-    return min(horizontal_distance / node[0], vertical_distance / node[0])
+    return  horizontal_distance(node, target_node) / node[0] + \
+            vertical_distance(node, target_node) / node[0]
 
 
 def get_closest_nodes(board_data):
@@ -58,13 +63,67 @@ def get_closest_nodes(board_data):
     return (start_node, end_node)
 
 
+def copy_node(node):
+    return [node[0], node[1], node[2]]
+
+
 def print_path(start_node, end_node):
     """
     Prints the moves for the path from the start_node to the end. Uses the stack
     size to move the furthest distance at a time. For now, move just one element
     of the stack.
+
+    Recursively prints the path taken between the two closest nodes.
     """
-    
+    horiz_dist = horizontal_distance(start_node, end_node)
+    vert_dist = vertical_distance(start_node, end_node)
+
+    if horiz_dist == 1 and vert_dist == 1:
+        print_boom(start_node[1], start_node[2])
+        return
+
+    # Moves horizontally first
+    if (horiz_dist < vert_dist or vert_dist == 1):
+        print("Move horiz 1")
+        #Get the direction
+        dir = 1
+        if (start_node[1] > end_node[1]):
+            dir = -1
+
+        #Get the distance we can move
+        if horiz_dist > start_node[0]:
+            num_spaces = start_node[0]
+        else:
+            num_spaces = horiz_dist
+
+        new_location = copy_node(start_node)
+        new_location[1] = start_node[1] + dir * num_spaces
+
+        #Print the move and print the rest of the path
+        print_move(1, start_node[1], start_node[2], new_location[1], new_location[2])
+        print_path(new_location, end_node)
+
+
+    # Moves vertically
+    else:
+        print("Move vert 1")
+        #Get the direction
+        dir = 1
+        if (start_node[2] > end_node[2]):
+            dir = -1
+
+        #Get the distance we can move
+        if vert_dist > start_node[0]:
+            num_spaces = start_node[0]
+        else:
+            num_spaces = vert_dist
+
+        new_location = copy_node(start_node)
+        new_location[2] = start_node[2] + dir * num_spaces
+
+        #Print the move and print the rest of the path
+        print_move(1, start_node[1], start_node[2], new_location[1], new_location[2])
+        print_path(new_location, end_node)
 
 
 if __name__ == '__main__':
