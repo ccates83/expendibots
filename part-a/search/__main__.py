0ef45eef260c5ca9cf911bd152c7f4de85f7afd9
Connor Cates <connorcates@Connors-MBP.fios-router.home>
2020-03-29 12:33:01 -0400

    wrote some codeto calculate how many black tokens willexplode at a givenlocation and what locations on the board have the highest number of tokens that explode.

--- part-a/search/__main__.py
+++ part-a/search/__main__.py
@@ -12,7 +12,8 @@ def main():
     print("#", data)
     print()
 
-    solve(data)
+    #solve(data)
+    print(find_optimal_locations(data));
 
 
 def solve(board_data):
@@ -28,12 +29,103 @@ def solve(board_data):
 
     solve(board_data)
 
+
 def horizontal_distance(node1, node2):
     return abs(node1[1] - node2[1])
 
 
 def vertical_distance(node1, node2):
     return abs(node1[2] - node2[2])
+
+
+def is_occupied_by_black(location, board_data):
+    print(location)
+    for black in board_data["black"]:
+        if location == (black[1], black[2]):
+            return True
+    return False
+
+
+def is_occupied_by_white(location, board_data):
+    for white in board_data["white"]:
+        if location == (white[1], white[2]):
+            return True
+    return False
+
+
+def is_occupied(location, board_data):
+    return  is_occupied_by_black(location, board_data) or \
+            is_occupied_by_white(location, board_data)
+
+def list_neighboring_empty_tiles(node, board_data):
+    """
+    Lists the locations surrounding the node.
+    """
+    neighbors = []
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if (i != 0 or j != 0) and \
+                not is_occupied_by_black((node[1] + i, node[2]  + j), board_data):
+                neighbors.append((node[1] + i, node[2]  + j))
+    return neighbors
+
+
+def count_occurances(list):
+    elements = {}
+
+    for elem in list:
+        if elem in elements:
+            elements[elem] += 1
+        else:
+            elements[elem] = 1
+    return elements
+
+
+def count_eliminated_tiles(explosion_location, board_data, already_exploded):
+    """
+    Calculates the number of black tiles that would blow up from a given location
+    """
+    # print(" -- current tile: ", explosion_location)
+    total = 0
+    for black in board_data["black"]:
+        # print("Checking ", explosion_location, " vs ", black)
+        # print(tiles_are_neighbors((black[1], black[2]), explosion_location))
+        if are_neighbors(black, (1, explosion_location[0], explosion_location[1])):
+            # print("Current: ", explosion_location, " -- next: ", black)
+            # print(already_exploded)
+            if black not in already_exploded:
+                # print("Exploding: ", black, "-----------------> +1")
+                already_exploded.append(black)
+                # print(already_exploded)
+                total += 1 + count_eliminated_tiles((black[1], black[2]), board_data, already_exploded)
+    return total
+
+
+def find_optimal_locations(board_data):
+    """
+    Find overlaps between as many neighbors of black as possible. This will blow
+    up the most black pieces per turn.
+    """
+    all_neighbors = []
+    for black in board_data["black"]:
+        for loc in list_neighboring_empty_tiles(black, board_data):
+            if loc not in all_neighbors:
+                all_neighbors.append(loc)
+
+    # Creates a list of the locations that will explode themost blackpieces
+    optimal_locations = []
+    most_explosions = 0
+    for loc in all_neighbors:
+        num_explosions = count_eliminated_tiles(loc, board_data, [])
+        if num_explosions > most_explosions:
+            most_explosions = num_explosions
+            optimal_locations = []
+            optimal_locations.append(loc)
+        elif num_explosions == most_explosions:
+            optimal_locations.append(loc)
+
+    return optimal_locations
+
 
 # Update this to check number of moves to being a NEIGHBOR not ontop
 def calculate_number_moves(node, target_node):
@@ -69,9 +161,13 @@ def get_closest_nodes(board_data):
 
 
 def are_neighbors(node1, node2):
-    return  horizontal_distance(node1, node2) == 1 and \
-            vertical_distance(node1, node2) == 1
+    return  horizontal_distance(node1, node2) <= 1 and \
+            vertical_distance(node1, node2) <= 1
 
+
+def tiles_are_neighbors(coord1, coord2):
+    return abs(coord1[0] - coord2[0]) <= 1 and \
+            abs(coord1[1] - coord2[1]) <= 1
 
 def explode(node, board_data):
     print_boom(node[1], node[2])
