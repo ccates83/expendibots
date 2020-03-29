f61aa5b3cecbe34cd0eb35b159e6020dea671cbc
Connor Cates <connorcates@Connors-MBP.fios-router.home>
2020-03-29 10:45:49 -0400

    solves a 1v1 in fewest moves - need to add heuristic to check if when we explode it doesnt make black win

--- part-a/search/__main__.py
+++ part-a/search/__main__.py
@@ -9,18 +9,24 @@ def main():
         data = json.load(file)
 
     # TODO: find and print winning action sequence
-    print(data)
+    print("#", data)
     print()
 
-    path = get_closest_nodes(data)
-    print_path(path[0], path[1])
+    solve(data)
 
 
-def solve():
+def solve(board_data):
     """
     Overarching function that outputs the moves needed to win the game.
     """
+    if (len(board_data["black"]) == 0):
+        print("# SOLVED")
+        return
 
+    path = get_closest_nodes(board_data)
+    execute_path(path[0], path[1], board_data)
+
+    solve(board_data)
 
 def horizontal_distance(node1, node2):
     return abs(node1[1] - node2[1])
@@ -29,7 +35,7 @@ def horizontal_distance(node1, node2):
 def vertical_distance(node1, node2):
     return abs(node1[2] - node2[2])
 
-
+# Update this to check number of moves to being a NEIGHBOR not ontop
 def calculate_number_moves(node, target_node):
     """
     Calculates the number of moves it would take the node to reach the distination
@@ -58,33 +64,55 @@ def get_closest_nodes(board_data):
                 start_node = white_piece
                 end_node = black_piece
                 fewest_moves = moves
-                print(fewest_moves)
 
     return (start_node, end_node)
+
+
+def are_neighbors(node1, node2):
+    return  horizontal_distance(node1, node2) == 1 and \
+            vertical_distance(node1, node2) == 1
+
+
+def explode(node, board_data):
+    print_boom(node[1], node[2])
+    if node in board_data["white"]:
+        board_data["white"].remove(node)
+    else:
+        board_data["black"].remove(node)
+
+    #explode any white piece neighbors
+    for white in board_data["white"]:
+        if (are_neighbors(node, white)):
+            explode(white, board_data)
+    for black in board_data["black"]:
+        if (are_neighbors(node, black)):
+            explode(black, board_data)
+
+    print("#", board_data)
 
 
 def copy_node(node):
     return [node[0], node[1], node[2]]
 
 
-def print_path(start_node, end_node):
+def execute_path(start_node, end_node, board_data):
     """
     Prints the moves for the path from the start_node to the end. Uses the stack
     size to move the furthest distance at a time. For now, move just one element
-    of the stack.
+    of the stack. Removes the node blown up from the list.
 
     Recursively prints the path taken between the two closest nodes.
     """
     horiz_dist = horizontal_distance(start_node, end_node)
     vert_dist = vertical_distance(start_node, end_node)
 
-    if horiz_dist == 1 and vert_dist == 1:
-        print_boom(start_node[1], start_node[2])
+    if are_neighbors(start_node, end_node):
+        explode(start_node, board_data)
         return
 
     # Moves horizontally first
     if (horiz_dist < vert_dist or vert_dist == 1):
-        print("Move horiz 1")
+        print("# Move horiz 1")
         #Get the direction
         dir = 1
         if (start_node[1] > end_node[1]):
@@ -96,17 +124,17 @@ def print_path(start_node, end_node):
         else:
             num_spaces = horiz_dist
 
-        new_location = copy_node(start_node)
-        new_location[1] = start_node[1] + dir * num_spaces
+        old_location = copy_node(start_node)
+        start_node[1] = start_node[1] + dir * num_spaces
 
         #Print the move and print the rest of the path
-        print_move(1, start_node[1], start_node[2], new_location[1], new_location[2])
-        print_path(new_location, end_node)
+        print_move(old_location[0], old_location[1], old_location[2], start_node[1], start_node[2])
+        execute_path(start_node, end_node, board_data)
 
 
     # Moves vertically
     else:
-        print("Move vert 1")
+        print("# Move vert 1")
         #Get the direction
         dir = 1
         if (start_node[2] > end_node[2]):
@@ -118,12 +146,12 @@ def print_path(start_node, end_node):
         else:
             num_spaces = vert_dist
 
-        new_location = copy_node(start_node)
-        new_location[2] = start_node[2] + dir * num_spaces
+        old_location = copy_node(start_node)
+        start_node[2] = start_node[2] + dir * num_spaces
 
         #Print the move and print the rest of the path
-        print_move(1, start_node[1], start_node[2], new_location[1], new_location[2])
-        print_path(new_location, end_node)
+        print_move(1, old_location[1], old_location[2], start_node[1], start_node[2])
+        execute_path(start_node, end_node, board_data)
 
 
 if __name__ == '__main__':
