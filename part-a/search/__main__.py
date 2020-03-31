1689ba8bbace9e388856417cf62d5a09b7344a93
jadsaliba <40390238+jadsaliba@users.noreply.github.com>
2020-03-31 23:26:34 +1100

    Update __main__.py
    updated main to test new search in Nodes class

--- part-a/search/__main__.py
+++ part-a/search/__main__.py
@@ -1,21 +1,30 @@
 import sys
 import json
 
-from search.util import print_move, print_boom, print_board
+from search.util import *
 from search.node import Node
-from search.solver import *
 
 def main():
     with open(sys.argv[1]) as file:
         data = json.load(file)
 
+    white = data["white"]
+    print(white)
+    print(data["white"][0])
+    black = data["black"]
+
     # TODO: find and print winning action sequence
+    print("ORIGINAL STATE:")
+    print(data)
     print_board(data)
-    solve(data)
 
 
+    print("Solving...")
 
 
+    root_node = Node(state=data, piece_number=0, parent=None,action=None,depth=0,path_cost=0,heuristic_cost=0)
+    print(root_node.state)
+    root_node.search()
 
 if __name__ == '__main__':
     main()
