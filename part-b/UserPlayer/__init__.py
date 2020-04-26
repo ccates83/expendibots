46cb0fe475593828d5633bd3b6c1cea98acf9481
Connor Cates <35548067+ccates83@users.noreply.github.com>
2020-04-26 18:02:52 -0400

    Created a simple user player class to play against our bot

--- part-b/UserPlayer/__init__.py
+++ part-b/UserPlayer/__init__.py
@@ -5,4 +5,4 @@
 # example import below, you can define it in another file and import
 # it into this module with the name 'Player':
 
-from frostbyte.player import ExamplePlayer as Player
+from UserPlayer.player import UserPlayer as Player
